66c6562abddda09f1764b7c4e3aeb7fc8acc5498
Peter Nitsche <peter.nitsche@gmail.com>
2024-11-21 15:19:54 +0100

    add material ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+import { Roboto } from "next/font/google";
+
+import { Container } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+
+import theme from "../theme";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
 });
 
 export const metadata: Metadata = {
@@ -25,8 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={`${roboto.variable}`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <Container maxWidth="md">{children}</Container>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,19 @@
 import { GetProductsDocument } from "@/__generated__/graphql";
 import { getClient } from "@/lib/client";
+import { Grid2 as Grid, Typography } from "@mui/material";
 
 export default async function Products() {
   const { data } = await getClient().query({ query: GetProductsDocument });
 
   return (
-    <ul>
+    <Grid container spacing={2} columns={{ xs: 2, sm: 4, md: 8 }}>
       {data.getLandingProducts.products?.map((product) => (
-        <li key={product.id}>{product.id}</li>
+        <Grid size={2} key={product.id}>
+          <Typography variant="subtitle1" gutterBottom noWrap>
+            {product.id}
+          </Typography>
+        </Grid>
       ))}
-    </ul>
+    </Grid>
   );
 }
