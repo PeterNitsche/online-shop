f2e9def63f07afea78971bd428f433e996d8ad3b
Peter Nitsche <peter.nitsche@gmail.com>
2024-11-21 16:11:35 +0100

    add product info to overview page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 import { GetProductsDocument } from "@/__generated__/graphql";
 import { getClient } from "@/lib/client";
-import { Grid2 as Grid, Typography } from "@mui/material";
+import { Box, Grid2 as Grid, Typography } from "@mui/material";
 
 export default async function Products() {
   const { data } = await getClient().query({ query: GetProductsDocument });
@@ -9,8 +11,19 @@ export default async function Products() {
     <Grid container spacing={2} columns={{ xs: 2, sm: 4, md: 8 }}>
       {data.getLandingProducts.products?.map((product) => (
         <Grid size={2} key={product.id}>
+          <Box height={100} width={100} position="relative">
+            <Image
+              src={product.images[0].secure_url!}
+              fill
+              alt={product.title || "Product"}
+            />
+          </Box>
           <Typography variant="subtitle1" gutterBottom noWrap>
-            {product.id}
+            Test Product X
+          </Typography>
+
+          <Typography variant="subtitle2" gutterBottom noWrap>
+            {`${product.price[0]} €`}
           </Typography>
         </Grid>
       ))}
