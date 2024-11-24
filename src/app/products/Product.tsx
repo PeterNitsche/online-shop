8b855cc6fecfb5d3f3a6650128ff330a8c1a882e
Peter Nitsche <peter.nitsche@gmail.com>
2024-11-24 12:55:27 +0100

    improve product grid

---
+++ src/app/products/Product.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { GetProductsQuery } from '@/__generated__/graphql';
+import {
+    Box, Card, CardActionArea, CardContent, Grid2 as Grid, Rating, Stack, Typography
+} from '@mui/material';
+
+type Products = NonNullable<GetProductsQuery["getLandingProducts"]["products"]>;
+type Product = Products[0];
+interface ProductProps {
+  product: Product;
+}
+
+export function Product({ product }: ProductProps) {
+  return (
+    <Grid size={2}>
+      <Card variant="outlined">
+        <CardActionArea>
+          <Link href={`product/${product.slug}`}>
+            <CardContent>
+              <Stack>
+                <Box height={150} width={"auto"} position="relative">
+                  <Image
+                    src={product.images[0].secure_url!}
+                    fill
+                    style={{ objectFit: "contain" }}
+                    alt={product.title || "Product"}
+                  />
+                </Box>
+                <Typography variant="subtitle1" gutterBottom noWrap>
+                  {product.title}
+                </Typography>
+
+                <Typography variant="subtitle2" gutterBottom noWrap>
+                  {`${product.price[0]} ${product.currency}`}
+                </Typography>
+
+                <Rating
+                  name="product-rating"
+                  readOnly
+                  disabled={!product.averageRating}
+                  value={product.averageRating || null}
+                />
+              </Stack>
+            </CardContent>
+          </Link>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
+}
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,63 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
+import { GetProductsDocument } from '@/__generated__/graphql';
+import { getClient } from '@/lib/client';
+import { Grid2 as Grid } from '@mui/material';
 
-import { GetProductsDocument, GetProductsQuery } from "@/__generated__/graphql";
-import { getClient } from "@/lib/client";
-import {
-  Box,
-  Card,
-  CardActionArea,
-  CardContent,
-  Grid2 as Grid,
-  Rating,
-  Stack,
-  Typography,
-} from "@mui/material";
-
-type Products = NonNullable<GetProductsQuery["getLandingProducts"]["products"]>;
-type Product = Products[0];
-interface ProductComponentProps {
-  product: Product;
-}
-
-function ProductComponent({ product }: ProductComponentProps) {
-  return (
-    <Grid size={2}>
-      <Card variant="outlined">
-        <CardActionArea>
-          <Link href={`product/${product.slug}`}>
-            <CardContent>
-              <Stack>
-                <Box height={150} width={150} position="relative">
-                  <Image
-                    src={product.images[0].secure_url!}
-                    fill
-                    objectFit="contain"
-                    alt={product.title || "Product"}
-                  />
-                </Box>
-                <Typography variant="subtitle1" gutterBottom noWrap>
-                  {product.title}
-                </Typography>
-
-                <Typography variant="subtitle2" gutterBottom noWrap>
-                  {`${product.price[0]} ${product.currency}`}
-                </Typography>
-
-                <Rating
-                  name="product-rating"
-                  readOnly
-                  disabled={!product.averageRating}
-                  value={product.averageRating || null}
-                />
-              </Stack>
-            </CardContent>
-          </Link>
-        </CardActionArea>
-      </Card>
-    </Grid>
-  );
-}
+import { Product } from './Product';
 
 export default async function Products() {
   const { data } = await getClient().query({ query: GetProductsDocument });
@@ -65,7 +10,7 @@ export default async function Products() {
   return (
     <Grid container spacing={2} columns={{ xs: 4, sm: 6, md: 8 }}>
       {data.getLandingProducts.products?.map((product) => (
-        <ProductComponent product={product} key={product.id} />
+        <Product product={product} key={product.id} />
       ))}
     </Grid>
   );
