30d1980814e4b73c44f41cd2c5199ae1a342e21c
Peter Nitsche <peter.nitsche@gmail.com>
2024-11-24 20:07:30 +0100

    query search param

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,19 +1,36 @@
 import { getFragmentData } from '@/__generated__';
-import { GetProductsDocument, ProductListItemFragmentDoc } from '@/__generated__/graphql';
+import {
+    GetProductsDocument, GetSearchProductsDocument, ProductListItemFragmentDoc
+} from '@/__generated__/graphql';
 import { getClient } from '@/lib/client';
 import { Grid2 as Grid, Stack } from '@mui/material';
 
 import { Product } from './Product';
 import { SearchInput } from './SearchInput';
 
-export default async function Products() {
-  const { data } = await getClient().query({ query: GetProductsDocument });
+interface ProductsProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function Products({ searchParams }: ProductsProps) {
+  const { query } = await searchParams;
+
+  const products =
+    query && typeof query === "string"
+      ? (
+          await getClient().query({
+            query: GetSearchProductsDocument,
+            variables: { term: query },
+          })
+        ).data.searchHomeProducts
+      : (await getClient().query({ query: GetProductsDocument })).data
+          .getLandingProducts.products;
 
   return (
     <Stack spacing={2} alignItems={"center"}>
       <SearchInput />
       <Grid container spacing={2} columns={{ xs: 4, sm: 6, md: 8 }}>
-        {data.getLandingProducts.products?.map((prd) => {
+        {products?.map((prd) => {
           const product = getFragmentData(ProductListItemFragmentDoc, prd);
           return <Product product={product} key={product.id} />;
         })}
