5a34cca09dbce007e6e7e49fa2d7faf43cc73ad8
Peter Nitsche <peter.nitsche@gmail.com>
2024-11-24 20:21:47 +0100

    add clear search button

--- src/app/products/SearchInput.tsx
+++ src/app/products/SearchInput.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useState } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
 
+import ClearIcon from '@mui/icons-material/Clear';
 import SearchIcon from '@mui/icons-material/Search';
-import { InputAdornment, TextField } from '@mui/material';
+import { IconButton, InputAdornment, TextField } from '@mui/material';
 
 export function SearchInput() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+
+  const [searchValue, setSearchValue] = useState(
+    searchParams.get("query") || ""
+  );
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
@@ -21,12 +27,18 @@ export function SearchInput() {
     replace(`${pathname}?${params.toString()}`);
   }, 300);
 
+  function clearSearch() {
+    setSearchValue("");
+    handleSearch("");
+  }
+
   return (
     <TextField
       label="Search products"
       sx={{ width: "80%", minWidth: "200px", maxWidth: "600px" }}
-      defaultValue={searchParams.get("query")?.toString()}
+      value={searchValue}
       onChange={(e) => {
+        setSearchValue(e.target.value);
         handleSearch(e.target.value);
       }}
       slotProps={{
@@ -34,6 +46,16 @@ export function SearchInput() {
           startAdornment: (
             <InputAdornment position="start">
               <SearchIcon />
+            </InputAdornment>
+          ),
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="Clear product search"
+                onClick={clearSearch}
+              >
+                <ClearIcon />
+              </IconButton>
             </InputAdornment>
           ),
         },
