49e9f79e46bf46ca73a635cf130f692442eb4858
Peter Nitsche <peter.nitsche@gmail.com>
2024-11-27 13:02:35 +0100

    fix empty box in carousel

--- src/app/product/[slug]/MediaCarousel.tsx
+++ src/app/product/[slug]/MediaCarousel.tsx
@@ -14,41 +14,42 @@ interface MediaCarouselProps {
   productTitle?: string | null;
 }
 
-export function MediaCarousel({ videoUrl, imageUrls, productTitle }: MediaCarouselProps) {
-  const responsive = {
-    all: {
-      breakpoint: { max: 3000, min: 0 },
-      items: 1,
-    },
-  };
+const responsive = {
+  all: {
+    breakpoint: { max: 3000, min: 0 },
+    items: 1,
+  },
+};
 
-  return (
-    <Carousel responsive={responsive}>
-      {imageUrls.map((imageUrl, i) => (
-        <Box key={imageUrl} height={500} position="relative">
-          <Image
-            src={imageUrl}
-            fill
-            priority={i === 0}
-            style={{ objectFit: 'contain' }}
-            alt={`${productTitle || 'Product'} Image ${i}`}
-            sizes={`${theme.breakpoints.values.lg}`}
-          />
-        </Box>
-      ))}
-      {videoUrl && (
-        <Box
-          key={videoUrl}
-          height={500}
-          position="relative"
-          justifyContent={'center'}
-          display={'flex'}
-        >
-          <video key={videoUrl} controls width={'auto'} height={'100%'}>
-            <source src={videoUrl} type="video/mp4" />
-          </video>
-        </Box>
-      )}
-    </Carousel>
-  );
+export function MediaCarousel({ videoUrl, imageUrls, productTitle }: MediaCarouselProps) {
+  const carouselItems = imageUrls.map((imageUrl, i) => (
+    <Box key={imageUrl} height={500} position="relative">
+      <Image
+        src={imageUrl}
+        fill
+        priority={i === 0}
+        style={{ objectFit: 'contain' }}
+        alt={`${productTitle || 'Product'} Image ${i}`}
+        sizes={`${theme.breakpoints.values.lg}`}
+      />
+    </Box>
+  ));
+
+  if (videoUrl) {
+    carouselItems.push(
+      <Box
+        key={videoUrl}
+        height={500}
+        position="relative"
+        justifyContent={'center'}
+        display={'flex'}
+      >
+        <video key={videoUrl} controls width={'auto'} height={'100%'}>
+          <source src={videoUrl} type="video/mp4" />
+        </video>
+      </Box>,
+    );
+  }
+
+  return <Carousel responsive={responsive}>{carouselItems}</Carousel>;
 }
